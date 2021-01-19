c1b09d4dc8c5cc2c067b34908e8797b5a118d338
kenorb <kenorb@users.noreply.github.com>
2021-01-19 23:27:44 +0000

    Optimize Indicator params (pf=0.68; dd=2.34; tt=2732)

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -6,8 +6,8 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_CCI_Params_M5 : CCIParams {
   Indi_CCI_Params_M5() : CCIParams(indi_cci_defaults, PERIOD_M5) {
-    applied_price = (ENUM_APPLIED_PRICE)6;
-    period = 6;
+    applied_price = (ENUM_APPLIED_PRICE)0;
+    period = 2;
     shift = 0;
   }
 } indi_cci_m5;
