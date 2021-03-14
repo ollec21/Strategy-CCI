8450794a97c7629557e50d01c99e45321f59cebe
ollec21 <9515639+ollec21@users.noreply.github.com>
2021-03-14 21:44:14 +0000

    Changes config

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -6,7 +6,7 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_CCI_Params_M15 : CCIParams {
   Indi_CCI_Params_M15() : CCIParams(indi_cci_defaults, PERIOD_M15) {
-    applied_price = (ENUM_APPLIED_PRICE)4;
+    applied_price = (ENUM_APPLIED_PRICE)3;
     period = 28;
     shift = 0;
   }
